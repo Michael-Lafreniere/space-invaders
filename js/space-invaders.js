e1ca9954cf32cd93f7fc8ac11e8e3aad0e4ab2e5
Michael Lafreniere <temike1@gmail.com>
2020-02-27 16:45:30 -0800

    Now records high score and allows you to proceed to the next level

--- js/space-invaders.js
+++ js/space-invaders.js
@@ -38,8 +38,7 @@ const GAME_STATE = {
   enemies: [],
   enemyLasers: [],
   gameOver: false,
-  gameWon: false,
-  gameStart: false
+  topScore: 0
 };
 
 // Array of available enemy ship color options:
@@ -49,6 +48,19 @@ const enemyShipList = ['Beige', 'Blue', 'Green', 'Pink', 'Yellow'];
 const gameContainer = document.querySelector('.game');
 
 // Utility functions:
+const restart = () => {
+  GAME_STATE.playerLevel = 0;
+  GAME_STATE.playerScore = 0;
+  writeLocalData();
+  window.location.reload();
+};
+
+const nextLevel = () => {
+  GAME_STATE.playerLevel += 1;
+  writeLocalData();
+  window.location.reload();
+};
+
 const setPosition = (element, x, y) => {
   element.style.transform = `translate(${x}px, ${y}px)`;
 };
@@ -69,6 +81,12 @@ const intersect = (rect1, rect2) => {
     rect2.bottom < rect1.top
   );
 };
+
+function random(min, max) {
+  if (min === undefined) min = 0;
+  if (max === undefined) max = 1;
+  return min + Math.random() * (max - min);
+}
 
 const onKeyDown = event => {
   switch (event.keyCode) {
@@ -134,7 +152,8 @@ const updateShots = dt => {
         if (intersect(rect1, rect2)) {
           destroyEnemy(enemy);
           destroyShot(laser);
-          GAME_STATE.playerScore += ENEMY_SHIP_VALUE;
+          GAME_STATE.playerScore +=
+            ENEMY_SHIP_VALUE + GAME_STATE.playerLevel * 1.05;
           break;
         }
       }
@@ -158,8 +177,7 @@ const createPlayer = container => {
 
 const destroyPlayer = () => {
   const player = document.querySelector('.player');
-  gameContainer.removeChild(player);
-  GAME_STATE.gameOver = true;
+  if (player) gameContainer.removeChild(player);
 };
 
 const updatePlayer = dt => {
@@ -187,7 +205,6 @@ const updatePlayer = dt => {
   // Set player position:
   const player = document.querySelector('.player');
   setPosition(player, GAME_STATE.playerX, GAME_STATE.playerY);
-  // console.log(GAME_STATE.playerX, GAME_STATE.playerY);
 };
 
 // Enemy functions:
@@ -197,7 +214,12 @@ const createEnemyShip = (x, y, index) => {
   enemyShip.src = `assets/Enemies/ship${color}_manned.png`;
   enemyShip.className = 'enemy';
   gameContainer.appendChild(enemyShip);
-  const enemy = { x, y, enemyShip };
+  const enemy = {
+    x,
+    y,
+    enemyShip,
+    cooldown: random(0.5, ENEMY_LASER_COOLDOWN)
+  };
   GAME_STATE.enemies.push(enemy);
   setPosition(enemyShip, x, y);
 };
@@ -205,8 +227,8 @@ const createEnemyShip = (x, y, index) => {
 const destroyEnemy = enemy => {
   gameContainer.removeChild(enemy.enemyShip);
   enemy.isDead = true;
-  const destroyEnemyShip = new Audio('assets/Sounds/phaseJump3.ogg');
-  destroyEnemyShip.play();
+  const destroyEnemyShipSound = new Audio('assets/Sounds/phaseJump3.ogg');
+  destroyEnemyShipSound.play();
 };
 
 const updateEnemyShips = dt => {
@@ -221,6 +243,11 @@ const updateEnemyShips = dt => {
       const x = enemy.x + dx;
       const y = enemy.y + dy;
       setPosition(enemy.enemyShip, x, y);
+      enemy.cooldown -= dt;
+      if (enemy.cooldown <= 0) {
+        createEnemyShot(enemy, x, y);
+        enemy.cooldown = ENEMY_LASER_COOLDOWN;
+      }
     }
 
     GAME_STATE.enemies = GAME_STATE.enemies.filter(enemy => !enemy.isDead);
@@ -228,13 +255,23 @@ const updateEnemyShips = dt => {
 };
 
 const createEnemyShot = (x, y) => {
-  const enemyLaser = document.createElement('img');
-  enemyLaser.src = 'assets/Effects/laserRed01.png';
-  enemyLaser.className = 'enemy-laser';
-  gameContainer.appendChild(enemyLaser);
-  const laser = { x, y, enemyLaser };
-  GAME_STATE.enemyLasers.push(laser);
-  setPosition(enemyLaser, x, y);
+  if (GAME_STATE.enemyLasers.length < 20) {
+    const enemyLaser = document.createElement('img');
+    enemyLaser.src = 'assets/Effects/laserRed01.png';
+    enemyLaser.className = 'enemy-laser';
+    gameContainer.appendChild(enemyLaser);
+    const shot = { x, y, enemyLaser };
+    GAME_STATE.enemyLasers.push(shot);
+    setPosition(enemyLaser, x, y);
+    // console.log(shot);
+  }
+};
+
+const destroyEnemyShot = shot => {
+  // gameContainer.removeChild(laser.laserContainer);
+  // laser.isDead = true;
+  gameContainer.removeChild(shot);
+  shot.isDead = true;
 };
 
 const updateEnemyShots = dt => {
@@ -243,21 +280,55 @@ const updateEnemyShots = dt => {
     const shot = shots[i];
     shot.y += dt * LASER_MAX_SPEED;
     if (shot.y > GAME_AREA_HEIGHT) {
-      destroyShot(shot);
+      // destroyEnemyShot(shot.enemyLaser);
     }
-    setPosition(shot, shot.x, shot.y);
-    const rect1 = laser.enemyLaser.getBoundingClientRect();
-    const rect2 = document.querySelector('.player').getBoundingClientRect();
-    if (intersect(rect1, rect2)) {
-      destroyPlayer();
-      destroyShot(shot);
-      break;
-    }
+    setPosition(shot.enemyLaser, shot.x, shot.y);
+    // const rect1 = laser.enemyLaser.getBoundingClientRect();
+    // const rect2 = document.querySelector('.player').getBoundingClientRect();
+    // if (intersect(rect1, rect2)) {
+    //   // The player was hit:
+    //   destroyPlayer();
+    //   destroyShot(shot);
+    //   GAME_STATE.gameOver = true;
+    //   break;
+    // }
+  }
+};
+
+// Cleanup game assets:
+const cleanupGame = () => {
+  const shots = GAME_STATE.lasers;
+  for (let i = 0; i < shots.length; i++) {
+    destroyShot(shots[i]);
+  }
+  const enemies = GAME_STATE.enemies;
+  for (let j = 0; j < enemies.length; j++) {
+    destroyEnemyShip(enemies[j]);
+  }
+  const enemyShots = GAME_STATE.enemyLasers;
+  for (let k = 0; k < enemyShots.length; k++) {
+    destroyShot(enemyShots[k]);
+  }
+  destroyPlayer();
+};
+
+const checkTopScore = () => {
+  if (GAME_STATE.playerScore > GAME_STATE.topScore) {
+    const highScore = new Audio('assets/Sounds/new_highscore.ogg');
+    GAME_STATE.topScore = GAME_STATE.playerScore;
+    setTimeout(function() {
+      highScore.play();
+    }, 2100);
+    writeLocalData();
+    const topScore = document.querySelector('.top-score');
+    topScore.textContent = `Top Score: ${GAME_STATE.topScore}`;
   }
 };
 
 // Main game update loop:
 const update = () => {
+  const topScore = document.querySelector('.top-score');
+  const level = document.querySelector('.level');
   const score = document.querySelector('.score');
 
   // Calculate the delta time between frames:
@@ -265,9 +336,23 @@ const update = () => {
   const dt = (currentTime - GAME_STATE.lastTime) / 1000;
 
   if (GAME_STATE.gameOver) {
+    cleanupGame();
     document.querySelector('.lost').style.display = 'block';
     const missionFailed = new Audio('assets/Sounds/mission_failed.ogg');
     missionFailed.play();
+    checkTopScore();
+    GAME_STATE.playerScore = 0;
+    GAME_STATE.playerLevel = 0;
+    writeLocalData();
+    return;
+  }
+
+  if (GAME_STATE.enemies.length === 0) {
+    cleanupGame();
+    const objective = new Audio('assets/Sounds/mission_completed.ogg');
+    objective.play();
+    document.querySelector('.won').style.display = 'block';
+    checkTopScore();
     return;
   }
 
@@ -275,28 +360,19 @@ const update = () => {
   updatePlayer(dt);
   updateShots(dt);
   updateEnemyShips(dt);
+  updateEnemyShots(dt);
 
-  score.textContent = `Score: ${GAME_STATE.playerScore}`;
+  topScore.textContent = `Top Score: ${GAME_STATE.topScore.toFixed(1)}`;
+  level.textContent = `Level: ${GAME_STATE.playerLevel + 1}`;
+  score.textContent = `Score: ${GAME_STATE.playerScore.toFixed(1)}`;
 
   // Update the lastTime to be the currentTime for next frame dt calcs:
   GAME_STATE.lastTime = currentTime;
   window.requestAnimationFrame(update);
 };
 
-// Game initialization function:
-const initialize = () => {
-  const gameContainer = document.querySelector('.game');
-  const level = document.querySelector('.level');
-  level.textContent = `Score: ${GAME_STATE.playerLevel}`;
-
-  // Register event listeners for key up/down:
-  window.addEventListener('keydown', onKeyDown);
-  window.addEventListener('keyup', onKeyUp);
-
-  // Create the player:
-  createPlayer(gameContainer);
-
-  // Create the enemies:
+// Creates all the enemy ships for a round:
+const createEnemyShips = () => {
   for (let j = 0; j < 3; j++) {
     const y = ENEMY_VERT_PADDING + j * ENEMY_VERT_SPACING;
     for (let i = 0; i < ENEMIES_PER_ROW; i++) {
@@ -306,11 +382,37 @@ const initialize = () => {
   }
 };
 
-// Kick off the update loop:
-const run = () => {
+// Reads all the previous levels, top scores, etc.
+const readLocalData = () => {
+  GAME_STATE.playerLevel = JSON.parse(localStorage.getItem('level')) || 0;
+  GAME_STATE.playerScore = JSON.parse(localStorage.getItem('score')) || 0;
+  GAME_STATE.topScore = JSON.parse(localStorage.getItem('top-score')) || 0;
+};
+
+const writeLocalData = () => {
+  localStorage.setItem('level', JSON.stringify(GAME_STATE.playerLevel));
+  localStorage.setItem('score', JSON.stringify(GAME_STATE.playerScore));
+  localStorage.setItem('top-score', JSON.stringify(GAME_STATE.topScore));
+};
+
+// Game initialization function:
+const initialize = () => {
+  // Read previous data:
+  readLocalData();
+
+  // Register event listeners for key up/down:
+  window.addEventListener('keydown', onKeyDown);
+  window.addEventListener('keyup', onKeyUp);
+
+  // Create the player:
+  createPlayer(gameContainer);
+
+  // Create the enemies:
+  createEnemyShips();
+
+  // start the game:
   window.requestAnimationFrame(update);
 };
 
 // Initialize and run the game:
 initialize();
-run();
